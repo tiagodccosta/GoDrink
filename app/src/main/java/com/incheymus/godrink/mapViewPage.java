32e42940d1c5fe5e3100aa3aac15655833b67e6e
tiagodccosta <costa1999>
2023-11-19 18:40:40 +0000

    ChooserPage, LandingPage, RecommendPage finished. Demo for second deliverable almost done.

---
+++ app/src/main/java/com/incheymus/godrink/mapViewPage.java
@@ -0,0 +1,14 @@
+package com.incheymus.godrink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mapViewPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_view_page);
+    }
+}
